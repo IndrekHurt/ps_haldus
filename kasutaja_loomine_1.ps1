1167899ca601c48e8f6eaed617a0f6321c609633
Indrek Hurt <indrek.hurt.001@voco.ee>
2024-12-02 12:34:26 +0200

    Add local user by script data-fixed

--- kasutaja_loomine_1.ps1
+++ kasutaja_loomine_1.ps1
@@ -1,1 +1,3 @@
-﻿
+﻿$userPassword = ConvertTo-SecureString 'qwerty' -AsPlainText -Force
+
+New-LocalUser 'kasutaja1' -Password $userPassword -FullName 'Esimene Kasutaja' -Description 'Local account'
