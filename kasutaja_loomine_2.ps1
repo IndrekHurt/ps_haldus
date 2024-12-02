33f2b0e63ce2eaac24fc93fb84ac27e9985d6d4e
Indrek Hurt <indrek.hurt.001@voco.ee>
2024-12-02 13:50:20 +0200

    Add local user with command line parameters data

---
+++ kasutaja_loomine_2.ps1
@@ -0,0 +1,9 @@
+﻿param(
+    $username,
+    $fullname,
+    $description
+)
+
+$userPassword = ConvertTo-SecureString 'werty' -AsPlainText -Force
+
+New-LocalUser $username -Password $userPassword -FullName $fullname -Description $description
