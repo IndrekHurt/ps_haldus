c74b8231288f9297734a0fdf93d33d89c336ad2b
Indrek Hurt <indrek.hurt.001@voco.ee>
2024-12-02 13:40:58 +0200

    Add local user with script manual output

---
+++ kasutaja_loomine_3.ps1
@@ -0,0 +1,7 @@
+﻿$users = Import-Csv "C:\Users\Indrek\ps_haldus\users.csv" -Encoding Default -Delimiter ";"
+foreach ($user in $users) {
+    $username = $user.Username
+    $fullname = $user.Fullname
+    $description = $user.Description
+    $password = $user.Password | ConvertTo-SecureString -AsPlainText -Force
+    New-LocalUser $username `        -Password $usePassword `        -FullName $fullname `        -Description $description        -ErrorAction SilentlyContinue}
